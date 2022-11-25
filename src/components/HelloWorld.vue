91d03d98b0bcd5e746dcc5703859a9e931282915
wb-hch339219 <wb-hch339219@alibaba-inc.com>
2022-11-25 20:57:57 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,26 @@
 <template>
-    <div>
-        <div style="text-align: center; font-size: 32px;">{{msg}}</div>
-        <div  style="text-align: center">{{moreMsg}}</div>
-    </div>
+  <div>
+    <div style="text-align: center; font-size: 32px;">{{ msg }}</div>
+    <div style="text-align: center">{{ moreMsg }}</div>
+  </div>
 </template>
 
 <script lang="ts">
 class Msg {
-    public msg: string = 'hello world';
-    GetMoreMsg(): string{
-        return'webpack+vue+typescript+less'
-    }
+  public msg: string = "hello world";
+  GetMoreMsg(): string {
+    return "webpack+vue+typescript+less111";
+  }
 }
 //创建msg对象
 let MsgObj = new Msg();
 export default {
-    name: 'Hello',
-    data() {
-        return {
-            msg: MsgObj.msg,
-            moreMsg: MsgObj.GetMoreMsg()
-        }
-    },
-}
+  name: "Hello",
+  data() {
+    return {
+      msg: MsgObj.msg,
+      moreMsg: MsgObj.GetMoreMsg(),
+    };
+  },
+};
 </script>
-
-
-
