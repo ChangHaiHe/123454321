398853c53b0eb172fb3a8cc1f8550cdda8860a9a
wb-hch339219 <wb-hch339219@alibaba-inc.com>
2022-11-26 03:28:01 +0800

    112233

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@
 class Msg {
   public msg: string = "hello world";
   GetMoreMsg(): string {
-    return "深夜广播，webpack+vue+typescript+less —— 优秀的昌海";
+    return "深夜广播，webpack+vue+typescript+less —— 昌海";
   }
 }
 //创建msg对象
